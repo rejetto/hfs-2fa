441c8ba33cbc89468000b8303db3d97e4702b6ec
Massimo Melina <a@rejetto.com>
2026-07-05 16:57:09 +0200

    image not working in hfs preview

--- dist/plugin.js
+++ dist/plugin.js
@@ -3,7 +3,7 @@ exports.description = "Implements OTP 2FA authentication"
 exports.apiRequired = 12.1 // Btn
 exports.frontend_js = ['main.js']
 exports.repo = "damienzonly/hfs-2fa"
-exports.preview = ["https://github.com/user-attachments/assets/58336a9c-7cec-428e-8a19-3203e7160702","https://github.com/user-attachments/assets/89e0c41b-6a74-4c6a-becc-072517c72d97","https://github.com/user-attachments/assets/8edb44a7-7949-4242-8fa7-de18da0e48e4","https://github.com/user-attachments/assets/e4f4ea64-ac6a-4274-84ea-9a1078c5f99f"]
+exports.preview = ["https://github.com/user-attachments/assets/c7514e29-eaf2-4901-85d2-f8919d0cbc79","https://github.com/user-attachments/assets/89e0c41b-6a74-4c6a-becc-072517c72d97","https://github.com/user-attachments/assets/8edb44a7-7949-4242-8fa7-de18da0e48e4","https://github.com/user-attachments/assets/e4f4ea64-ac6a-4274-84ea-9a1078c5f99f"]
 
 exports.config = {
     issuer: {
